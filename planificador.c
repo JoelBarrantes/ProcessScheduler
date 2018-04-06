fc9792d192f5c5bf565d898f168287844fe52085
Joel Barrantes <32499639+JoelBarrantes@users.noreply.github.com>
2018-04-06 02:12:43 -0600

    Add files via upload

--- planificador.c
+++ planificador.c
@@ -8,10 +8,14 @@
 #include <unistd.h>
 #include <time.h>
 #include <math.h>
+#include <termios.h>
 
 
 int GLOBAL_PID=0;
-
+int SECS = 0;
+int SECS_L = 0;
+int CPU_WAIT = 0;
+pthread_mutex_t mutex;
 
 
 struct PCB{
@@ -26,6 +30,7 @@ struct process
     int *status;
     int burst;     
     int curr_burst;
+	int begin_time;
     int TAT;
     int WT;
 };
@@ -42,7 +47,43 @@ struct thread_args{
 	int algorithm;
 	int quantum;
 };
+	
 
+char getch() {
+        char buf = 0;
+        struct termios old = {0};
+        if (tcgetattr(0, &old) < 0)
+                perror("tcsetattr()");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &old) < 0)
+                perror("tcsetattr ICANON");
+        if (read(0, &buf, 1) < 0)
+                perror ("read()");
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+        if (tcsetattr(0, TCSADRAIN, &old) < 0)
+                perror ("tcsetattr ~ICANON");
+        return (buf);
+}
+
+
+void *count(void *args){
+	
+	int *finish = (int *)args;
+	while (*finish != 1){
+		pthread_mutex_lock(&mutex);	
+		SECS++;
+		printf("%d",SECS);
+		fflush(stdout);		
+		pthread_mutex_unlock(&mutex);
+		sleep(1);
+		
+	}
+	return NULL;
+}
 
 void *CPU_scheduler_thread(void *arguments){
 
@@ -52,13 +93,30 @@ void *CPU_scheduler_thread(void *arguments){
 	int quantum = (int)args->quantum;
 	struct queue *p_queue = (struct queue *)args->ready_queue;
 	struct queue *d_queue = (struct queue *)args->done_queue;
-	printf("Waiting for a process...\n");
+	
+	int finish = 0;
+	
+	pthread_t counter;
+
+	printf("CPU Scheduler ready. Waiting for a process...\n");
 	fflush(stdout);
+	/*	
+	if(pthread_create(&counter, NULL, count, &finish)){
+		fprintf(stderr, "Error creating thread\n");
+		return NULL;
+	}
+	*/
+	
 	while(*status_ptr != 1){
 	
 		runCPUScheduler(p_queue,d_queue, algorithm, quantum);
-
+		
 	}
+	finish = 1;
+	/*pthread_join(counter, NULL);*/
+	display_times(d_queue);	
+	
+	
 	
 	return NULL;
     
@@ -84,18 +142,23 @@ void *read_key(void *arguments){
     int *status_ptr = (int *)args->status;
 	
 	while(*status_ptr != 1){	
-		int ch = getchar();
+		int ch = getch();
 	
 		if (ch < 0){
-			if (ferror(stdin)){
 				clearerr(stdin);
+			if (ferror(stdin)){
 			}
 		}
+
 		if (ch == 27){
-		
+			
 			++(*status_ptr);
+			printf("\nFinishing...\n");
+			fflush(stdout);
+			
 		}
 		if (ch == 9){
+
 			display_queue(p_queue);
 		}
 	}
@@ -106,11 +169,54 @@ void *read_key(void *arguments){
 int display_queue(struct queue *ready_queue){
 	struct list_head *pos;
 	struct queue *queue_tmp;
+	printf("\nReady Queue: \n");
 	list_for_each(pos, &ready_queue -> list){
 		 queue_tmp = list_entry(pos, struct queue, list);
-		 printf("PID: %d | TOTAL BURST: %d | REMAINING BURST: %d | PRIORITY: %d\n",queue_tmp -> p.process_pcb.PID, queue_tmp -> 			 p.burst,  queue_tmp -> p.curr_burst, queue_tmp-> p.process_pcb.priority);
-		 fflush(stdout);
+		 printf("PID: %d | Total Burst: %d | Remaining Burst: %d | Priority: %d\n",queue_tmp -> p.process_pcb.PID, queue_tmp -> 			 p.burst,  queue_tmp -> p.curr_burst, queue_tmp-> p.process_pcb.priority);
+		 
 	}
+	printf("\n");
+	fflush(stdout);
+	return 0;
+
+}
+
+int display_times(struct queue *done_queue){
+	
+	//pthread_mutex_lock(&mutex);
+	int p_quantity = 0;
+	int cpu_wait = 0;	
+	float mu_tat = 0;
+	float mu_wt = 0;
+
+	int total_burst = 0;	
+	struct list_head *pos;
+	struct queue *queue_tmp;
+		
+	printf("\nFinished Processes Table: \n");
+	list_for_each(pos, &done_queue -> list){
+
+		p_quantity++;
+		queue_tmp = list_entry(pos, struct queue, list);
+		printf("PID: %d | Burst: %d | Arrival Time: %d |Turn-Around Time: %d | Waiting Time: %d\n", queue_tmp -> p.process_pcb.PID, queue_tmp -> p.burst ,queue_tmp -> p.begin_time,  queue_tmp -> p.TAT,  queue_tmp -> p.WT);
+		fflush(stdout);
+		
+		total_burst = total_burst + (queue_tmp -> p.burst);
+		mu_tat = mu_tat + (queue_tmp -> p.TAT);
+		mu_wt = mu_wt + (queue_tmp -> p.WT);
+
+	}
+	cpu_wait = CPU_WAIT;
+	mu_tat = mu_tat / p_quantity;
+	mu_wt = mu_wt / p_quantity;
+	
+	printf("\nCPU Wait: %d\n", cpu_wait );
+	printf("Processes Finished: %d\n", p_quantity );
+
+	printf("Average Turn-Around Time: %f\n", mu_tat );	
+
+	printf("Average Waiting Time: %f\n", mu_wt );	
+	//pthread_mutex_unlock(&mutex);	
 	return 0;
 
 }
@@ -118,11 +224,12 @@ int display_queue(struct queue *ready_queue){
 int runJobScheduler(struct queue *ready_queue){
 	
 	
+	
 	srand(time(NULL));
 	int burst = (rand() % 10)+1;
 	int priority = (rand() % 5)+1;
-	int wait_time = (rand() % 1)+3;
-
+	int wait_time = (rand() % 3)+3;
+	
 	struct PCB pcb;
 	pcb.PID = GLOBAL_PID++;
 	pcb.priority = priority;
@@ -135,6 +242,10 @@ int runJobScheduler(struct queue *ready_queue){
 	proc.status = &pcb.status;
 	proc.burst = burst;
 	proc.curr_burst = burst;
+	//pthread_mutex_lock(&mutex);	
+	proc.begin_time = SECS_L;
+	printf("+%d+", SECS_L);
+	//pthread_mutex_unlock(&mutex);
 	proc.TAT = 0;
 	proc.WT = 0;
 	
@@ -145,15 +256,18 @@ int runJobScheduler(struct queue *ready_queue){
 	//INIT_LIST_HEAD(&queue_tmp->list);
 	queue_tmp->p = proc;
 	list_add_tail(&(queue_tmp->list), &(ready_queue -> list));	
+	
 	sleep(wait_time);
+	SECS_L = SECS_L + wait_time;
 	
 	
 	return 0;
 };
-
+	
 int runCPUScheduler(struct queue *ready_queue, struct queue *done_queue, int algorithm,int quantum){
 	
 	if (list_empty(&ready_queue -> list) == 0) {
+		//pthread_mutex_lock(&mutex);			
 		struct list_head *pos;
 		struct queue *queue_tmp;
 		struct queue *min_p = (list_entry(ready_queue->list.next, struct queue, list)); 
@@ -182,32 +296,57 @@ int runCPUScheduler(struct queue *ready_queue, struct queue *done_queue, int alg
 		
 		
 		printf("A process has started: ");
-		printf("PID: %d | TOTAL BURST: %d | REMAINING BURST: %d | PRIORITY: %d\n",min_p->p.process_pcb.PID,  min_p -> 				p.burst, min_p -> p.curr_burst, min_p -> p.process_pcb.priority);
+		printf("PID: %d | Total Burst: %d | Remaining Burst: %d | Priority: %d\n",min_p->p.process_pcb.PID,  min_p -> p.burst, min_p -> p.curr_burst, min_p -> p.process_pcb.priority);
 		fflush(stdout);	
 		
+
+
 		if (algorithm==3){
 			if (min_p ->p.curr_burst < quantum){
+			
+				//pthread_mutex_unlock(&mutex);
+
 				sleep(min_p ->p.burst);
+				SECS = SECS + min_p -> p.burst;				
+				//pthread_mutex_lock(&mutex);
 				min_p -> p.curr_burst = 0;			
 			}
 			else {
+				//pthread_mutex_unlock(&mutex);
 				sleep(quantum);
+				//pthread_mutex_lock(&mutex);
+				
+				SECS = SECS + quantum;				
 				min_p -> p.curr_burst = (min_p -> p.curr_burst) - quantum;
 				list_move_tail(&(min_p->list), &(ready_queue -> list));
 			}
 		}
 		else{
+			//pthread_mutex_unlock(&mutex);
 			sleep(min_p ->p.burst);
+			SECS = SECS + min_p -> p.burst;
+			//pthread_mutex_lock(&mutex);
 			min_p -> p.curr_burst = 0;
 		}
-
+		
 		if (min_p -> p.curr_burst == 0){
-			list_move(&(min_p->list), &(done_queue -> list));
-			printf("Process with PID %d finished. Waiting for a process...\n", min_p -> p.process_pcb.PID);
+			printf("%d", SECS);
+			min_p -> p.TAT = SECS - (min_p -> p.begin_time);
+					
+			min_p -> p.WT = (min_p -> p.TAT) - (min_p -> p.burst);
+			list_move_tail(&(min_p->list), &(done_queue -> list));
+			printf("Process with PID %d finished\n", min_p -> p.process_pcb.PID);
 			fflush(stdout);
 		}	
+		
+		//pthread_mutex_unlock(&mutex);
+
+	} else{
+		SECS++;
+		CPU_WAIT++;
+		sleep(1);
 	}
-	return 0;
+ 	return 0;
 
 };
 
@@ -315,19 +454,27 @@ int main(int argc, char **argv)
 	args.done_queue = &done_queue;
 	args.algorithm = algorithm;
 	args.quantum = quantum;
+
+
+	
+
+		
 	pthread_t job_scheduler_thread;
 	if(pthread_create(&job_scheduler_thread, NULL, Job_scheduler_thread, &args)){
 		fprintf(stderr, "Error creating thread\n");
 		return 1;
 	}
 
+	while(list_empty(&ready_queue.list) == 1) {
+		
+	}	
+		
 	pthread_t cpu_scheduler_thread;
 	if(pthread_create(&cpu_scheduler_thread, NULL, CPU_scheduler_thread, &args)){
 		fprintf(stderr, "Error creating thread\n");
 		return 1;
 	}
 
-	
 	pthread_t key_reader;
 	if(pthread_create(&key_reader, NULL, read_key, &args)){
 		fprintf(stderr, "Error creating thread\n");
--- planificador.h
+++ planificador.h
@@ -1,16 +1,18 @@
 #ifndef PLANIFICADOR_H
 #define PLANIFICADOR_H
  
+#include <unistd.h>
+#include <termios.h>
 
 extern int GLOBAL_PID; 
+extern int SECS;
  
 struct PCB;
 struct process;
 struct queue;
 struct thread_args;
 
-
-
+int display_times(struct queue *done_queue);
 
 void *CPU_scheduler_thread(void *status);
 int runCPUScheduler();
@@ -25,7 +27,11 @@ void *read_key(void *status);
 
 int display_queue(struct queue *ready_queue);
 
+
+char getch();
 int main();
+
+
  
  
 #endif
